535c554f99ae94354b46d4ec74ce4392210f8182
eric-lab-star <cyon256@icloud.com>
2023-06-05 10:33:01 +0900

    Lists

--- DSAL/code/main.go
+++ DSAL/code/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
-func main(){
-	fmt.Println("hello world")
+import (
+	"container/list"
+	"fmt"
+)
 
+func main() {
+	var intList = list.List{}
+	intList.PushBack(11)
+	intList.PushBack(23)
+	intList.PushBack(34)
+
+	for el := intList.Front(); el != nil; el = el.Next() {
+		fmt.Println(el.Value.(int))
+	}
 }
