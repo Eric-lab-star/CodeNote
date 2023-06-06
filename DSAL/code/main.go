10c3a8a6f2f5f1d40bb1b180b92310bb8a347968
eric-lab-star <cyon256@icloud.com>
2023-06-06 17:09:10 +0900

    section2 minmum heap

--- DSAL/code/main.go
+++ DSAL/code/main.go
@@ -1,12 +1,38 @@
-// Tuples
+// Heap
 package main
 
-import "fmt"
+import (
+	"container/heap"
+	"fmt"
+)
 
 func main() {
-	fmt.Println(timeTable(2))
+	var intHeap *IntegerHeap = &IntegerHeap{1, 9, 4}
+	heap.Init(intHeap)
+	heap.Push(intHeap, 2)
+	fmt.Printf("minimum: %d\n", (*intHeap)[0])
+	for intHeap.Len() > 0 {
+		fmt.Printf("%d \n", heap.Pop(intHeap))
+	}
+
 }
 
-func timeTable(a int) (int, int) {
-	return a, a * 2
+type IntegerHeap []int
+
+// implementing sort.Interface which is required for heap.Interface
+func (iheap IntegerHeap) Len() int           { return len(iheap) }          // length of iheap
+func (iheap IntegerHeap) Less(i, j int) bool { return iheap[i] > iheap[j] } //ascending order
+func (iheap IntegerHeap) Swap(i, j int)      { iheap[i], iheap[j] = iheap[j], iheap[i] }
+
+// implementing heap.Interface
+func (iheap *IntegerHeap) Push(heapintf interface{}) {
+	*iheap = append(*iheap, heapintf.(int))
+}
+
+func (iheap *IntegerHeap) Pop() interface{} {
+	previous := *iheap
+	n := len(previous)
+	x1 := previous[n-1]
+	*iheap = previous[0 : n-1]
+	return x1
 }
--- DSAL/section1/chapter1.md
+++ DSAL/section1/chapter1.md
@@ -24,3 +24,12 @@
 ### Tuples
 
 Tuples are typically immutable sequential collections.
+
+### Heaps
+
+A heap is a data structure that is based on the heap property.
+The heap data structure is used in selection, graph, and k-way merge algorithims.
+Operations such as finding, merging, insertion, key changes and deleting are performed on heaps.
+
+if the order is descending, it is referred to as maximum heap; otherwise, it's a minimumheap.
+It is paritally ordered data structure.
