e548fecf47c43102b3fe31f0b4ec6c3d31605bc1
eric-lab-star <cyon256@icloud.com>
2023-06-06 15:09:54 +0900

    section1 list

--- DSAL/code/main.go
+++ DSAL/code/main.go
@@ -1,63 +1,18 @@
-// main package has examples shown
-// in Hands-On Data Structures and algorithms with Go book
 package main
 
-// importing fmt package
 import (
+	"container/list"
 	"fmt"
 )
 
-// IDrawShape interface
-type IDrawShape interface {
-	drawShape(x [5]float32, y [5]float32)
-}
-
-type DrawShape2 struct{}
-
-func (DrawShape2) drawShape(x [5]float32, y [5]float32) {
-	fmt.Println("Drawing Shape2")
-}
-
-// DrawShape struct
-type DrawShape struct{}
-
-// DrawShape struct has method draw Shape with float x and y coordinates
-func (drawShape DrawShape) drawShape(x [5]float32, y [5]float32) {
-	fmt.Println("Drawing Shape")
-}
-
-// IContour interace
-type IContour interface {
-	drawContour(x [5]float32, y [5]float32)
-	resizeByFactor(factor int)
-}
-
-// DrawContour method resizeByFactor given factor
-
-// DrawContour struct
-type DrawContour struct {
-	x      [5]float32
-	y      [5]float32
-	shape  IDrawShape
-	factor int
-}
-
-// DrawContour method drawContour given the coordinates
-func (contour DrawContour) drawContour(x [5]float32, y [5]float32) {
-	fmt.Println("Drawing Contour")
-	contour.shape.drawShape(contour.x, contour.y)
-}
-
-// DrawContour method resizeByFactor given factor
-func (contour DrawContour) resizeByFactor(factor int) {
-	contour.factor = factor
-}
-
-// main method
 func main() {
-	var x = [5]float32{1, 2, 3, 4, 5}
-	var y = [5]float32{1, 2, 3, 4, 5}
-	var contour IContour = DrawContour{x, y, DrawShape{}, 2}
-	contour.drawContour(x, y)
-	contour.resizeByFactor(2)
+	intList := list.New()
+	intList.PushBack(1)
+	intList.PushBack(2)
+	intList.PushBack(3)
+	intList.PushBack(4)
+
+	for el := intList.Front(); el != nil; el = el.Next() {
+		fmt.Println(el.Value.(int))
+	}
 }
---
+++ DSAL/section1/chapter1.md
@@ -0,0 +1,18 @@
+# Data Structure and Algorithms
+
+## Classification of data Structure and Structural design patterns
+
+- Linear
+  lists/ sets/ tuples/ queues/ stacks/ heap
+- Non-Linera
+  Trees/ tables/ containers
+- Homogenous
+  2d arrays/ MultiDArray
+- Heterogenous
+  Linked Lists/ Ordered Lists/ Unordered Lists
+- Dynamic
+  Dictionaries/ TreeSets/ Sequences
+
+## List, Tuples and Heaps
+
+### Lists
