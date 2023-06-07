29c40b33192b0150c84f29684a0c5858b54cef90
eric-lab-star <cyon256@icloud.com>
2023-06-07 14:40:41 +0900

    bridge pattern: md

--- DSAL/code/main.js
+++ DSAL/code/main.js
@@ -1,1 +1,43 @@
-console.log("ts file");
+// bridge pattern
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var Abstraction = /** @class */ (function () {
+    function Abstraction(printer) {
+        this.implementation = printer;
+    }
+    Abstraction.prototype.print = function () {
+        this.implementation.Print();
+    };
+    return Abstraction;
+}());
+var mac = /** @class */ (function (_super) {
+    __extends(mac, _super);
+    function mac(printer) {
+        return _super.call(this, printer) || this;
+    }
+    return mac;
+}(Abstraction));
+var php = /** @class */ (function () {
+    function php() {
+    }
+    php.prototype.Print = function () {
+        console.log("printing files from php");
+    };
+    return php;
+}());
+var printer = new php();
+var pc = new mac(printer);
+pc.print();
