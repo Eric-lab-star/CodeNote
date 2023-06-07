5c9190b7e87292ca2456aa0b36abc209821efa50
eric-lab-star <cyon256@icloud.com>
2023-06-08 08:37:58 +0900

    created javaDoc/chapter2.md

--- myJavaDoc/code/Hello.java
+++ myJavaDoc/code/Hello.java
@@ -1,10 +1,13 @@
-/*
- * Date
- * Source
- *
- */
-class Hello {
+import java.util.Scanner;
+
+public class Hello {
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Insert one digit number");
+		String input = scanner.nextLine();
+		int num = Integer.parseInt(input);
+		System.out.println("received value: " + input);
+		System.out.printf("num=%d%n", num);
+		scanner.close();
 	}
 }
