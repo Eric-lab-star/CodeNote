072869074cac80b3ffc31cc9bb856f8c6509e589
eric-lab-star <cyon256@icloud.com>
2023-06-12 13:38:44 +0900

    composite: ts

--- DSAL/code/main.js
+++ DSAL/code/main.js
@@ -1,65 +1,186 @@
 // composite
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
-        return extendStatics(d, b);
+function _array_like_to_array(arr, len) {
+    if (len == null || len > arr.length) len = arr.length;
+    for(var i = 0, arr2 = new Array(len); i < len; i++)arr2[i] = arr[i];
+    return arr2;
+}
+function _array_without_holes(arr) {
+    if (Array.isArray(arr)) return _array_like_to_array(arr);
+}
+function _assert_this_initialized(self) {
+    if (self === void 0) {
+        throw new ReferenceError("this hasn't been initialised - super() hasn't been called");
+    }
+    return self;
+}
+function _class_call_check(instance, Constructor) {
+    if (!(instance instanceof Constructor)) {
+        throw new TypeError("Cannot call a class as a function");
+    }
+}
+function _defineProperties(target, props) {
+    for(var i = 0; i < props.length; i++){
+        var descriptor = props[i];
+        descriptor.enumerable = descriptor.enumerable || false;
+        descriptor.configurable = true;
+        if ("value" in descriptor) descriptor.writable = true;
+        Object.defineProperty(target, descriptor.key, descriptor);
+    }
+}
+function _create_class(Constructor, protoProps, staticProps) {
+    if (protoProps) _defineProperties(Constructor.prototype, protoProps);
+    if (staticProps) _defineProperties(Constructor, staticProps);
+    return Constructor;
+}
+function _define_property(obj, key, value) {
+    if (key in obj) {
+        Object.defineProperty(obj, key, {
+            value: value,
+            enumerable: true,
+            configurable: true,
+            writable: true
+        });
+    } else {
+        obj[key] = value;
+    }
+    return obj;
+}
+function _get_prototype_of(o) {
+    _get_prototype_of = Object.setPrototypeOf ? Object.getPrototypeOf : function getPrototypeOf(o) {
+        return o.__proto__ || Object.getPrototypeOf(o);
     };
-    return function (d, b) {
-        if (typeof b !== "function" && b !== null)
-            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
-var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
-    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
-        if (ar || !(i in from)) {
-            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
-            ar[i] = from[i];
+    return _get_prototype_of(o);
+}
+function _inherits(subClass, superClass) {
+    if (typeof superClass !== "function" && superClass !== null) {
+        throw new TypeError("Super expression must either be null or a function");
+    }
+    subClass.prototype = Object.create(superClass && superClass.prototype, {
+        constructor: {
+            value: subClass,
+            writable: true,
+            configurable: true
         }
+    });
+    if (superClass) _set_prototype_of(subClass, superClass);
+}
+function _iterable_to_array(iter) {
+    if (typeof Symbol !== "undefined" && iter[Symbol.iterator] != null || iter["@@iterator"] != null) return Array.from(iter);
+}
+function _non_iterable_spread() {
+    throw new TypeError("Invalid attempt to spread non-iterable instance.\\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.");
+}
+function _possible_constructor_return(self, call) {
+    if (call && (_type_of(call) === "object" || typeof call === "function")) {
+        return call;
     }
-    return to.concat(ar || Array.prototype.slice.call(from));
-};
-var Searcher = /** @class */ (function () {
-    function Searcher(name) {
-        this.name = name;
-    }
-    return Searcher;
-}());
-var Leaf = /** @class */ (function (_super) {
-    __extends(Leaf, _super);
-    function Leaf(name) {
-        return _super.call(this, name) || this;
-    }
-    Leaf.prototype.search = function () {
-        console.log(this.name);
+    return _assert_this_initialized(self);
+}
+function _set_prototype_of(o, p) {
+    _set_prototype_of = Object.setPrototypeOf || function setPrototypeOf(o, p) {
+        o.__proto__ = p;
+        return o;
     };
+    return _set_prototype_of(o, p);
+}
+function _to_consumable_array(arr) {
+    return _array_without_holes(arr) || _iterable_to_array(arr) || _unsupported_iterable_to_array(arr) || _non_iterable_spread();
+}
+function _type_of(obj) {
+    "@swc/helpers - typeof";
+    return obj && typeof Symbol !== "undefined" && obj.constructor === Symbol ? "symbol" : typeof obj;
+}
+function _unsupported_iterable_to_array(o, minLen) {
+    if (!o) return;
+    if (typeof o === "string") return _array_like_to_array(o, minLen);
+    var n = Object.prototype.toString.call(o).slice(8, -1);
+    if (n === "Object" && o.constructor) n = o.constructor.name;
+    if (n === "Map" || n === "Set") return Array.from(n);
+    if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _array_like_to_array(o, minLen);
+}
+function _is_native_reflect_construct() {
+    if (typeof Reflect === "undefined" || !Reflect.construct) return false;
+    if (Reflect.construct.sham) return false;
+    if (typeof Proxy === "function") return true;
+    try {
+        Boolean.prototype.valueOf.call(Reflect.construct(Boolean, [], function() {}));
+        return true;
+    } catch (e) {
+        return false;
+    }
+}
+function _create_super(Derived) {
+    var hasNativeReflectConstruct = _is_native_reflect_construct();
+    return function _createSuperInternal() {
+        var Super = _get_prototype_of(Derived), result;
+        if (hasNativeReflectConstruct) {
+            var NewTarget = _get_prototype_of(this).constructor;
+            result = Reflect.construct(Super, arguments, NewTarget);
+        } else {
+            result = Super.apply(this, arguments);
+        }
+        return _possible_constructor_return(this, result);
+    };
+}
+var Searcher = function Searcher(name) {
+    "use strict";
+    _class_call_check(this, Searcher);
+    _define_property(this, "name", void 0);
+    this.name = name;
+};
+var Leaf = /*#__PURE__*/ function(Searcher) {
+    "use strict";
+    _inherits(Leaf, Searcher);
+    var _super = _create_super(Leaf);
+    function Leaf(name) {
+        _class_call_check(this, Leaf);
+        return _super.call(this, name);
+    }
+    _create_class(Leaf, [
+        {
+            key: "search",
+            value: function search() {
+                console.log(this.name);
+            }
+        }
+    ]);
     return Leaf;
-}(Searcher));
-var Tree = /** @class */ (function (_super) {
-    __extends(Tree, _super);
+}(Searcher);
+var Tree = /*#__PURE__*/ function(Searcher) {
+    "use strict";
+    _inherits(Tree, Searcher);
+    var _super = _create_super(Tree);
     function Tree(name) {
-        var _this = _super.call(this, name) || this;
-        _this.searchers = [];
+        _class_call_check(this, Tree);
+        var _this;
+        _this = _super.call(this, name);
+        _define_property(_assert_this_initialized(_this), "searchers", []);
         return _this;
     }
-    Tree.prototype.search = function () {
-        this.searchers.forEach(function (s) { return s.search(); });
-    };
-    Tree.prototype.add = function () {
-        var i = [];
-        for (var _i = 0; _i < arguments.length; _i++) {
-            i[_i] = arguments[_i];
+    _create_class(Tree, [
+        {
+            key: "search",
+            value: function search() {
+                this.searchers.forEach(function(s) {
+                    return s.search();
+                });
+            }
+        },
+        {
+            key: "add",
+            value: function add() {
+                for(var _len = arguments.length, i = new Array(_len), _key = 0; _key < _len; _key++){
+                    i[_key] = arguments[_key];
+                }
+                this.searchers = _to_consumable_array(this.searchers).concat(_to_consumable_array(i));
+            }
         }
-        this.searchers = __spreadArray(__spreadArray([], this.searchers, true), i, true);
-    };
+    ]);
     return Tree;
-}(Searcher));
+}(Searcher);
 var file1 = new Leaf("file1");
 var file2 = new Leaf("file2");
 var tree = new Tree("Tree");
 tree.add(file1, file2);
 tree.search();
+
