3c5b939480406e3255d85ea5e72b6aa31dd90891
eric-lab-star <cyon256@icloud.com>
2023-06-12 15:18:56 +0900

    facade: go

--- DSAL/code/main.go
+++ DSAL/code/main.go
@@ -1,44 +1,164 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+)
 
-type IPizza interface {
-	getPrice() int
+// walletFacade
+
+type WalletFacade struct {
+	account      *Account
+	wallet       *Wallet
+	securityCode *SecurityCode
+	notification *Notification
+	ledger       *Ledger
 }
 
-type VeggeMania struct {
+func newWalletFacade(accountID string, code int) *WalletFacade {
+	fmt.Println("Starting create account")
+	walletFacacde := &WalletFacade{
+		account:      newAccount(accountID),
+		securityCode: newSecurityCode(code),
+		wallet:       newWallet(),
+		notification: &Notification{},
+		ledger:       &Ledger{},
+	}
+	fmt.Println("Account created")
+	return walletFacacde
 }
 
-func (p *VeggeMania) getPrice() int {
-	return 15
+func (w *WalletFacade) addMoneyToWallet(accountID string, securityCode int, amount int) error {
+	fmt.Println("Starting add money to wallet")
+	err := w.account.checkAccount(accountID)
+	if err != nil {
+		return err
+	}
+	err = w.securityCode.checkCode(securityCode)
+	if err != nil {
+		return err
+	}
+	w.wallet.creditBalance(amount)
+	w.notification.sendWalletCreditNotification()
+	w.ledger.makeEntry(accountID, "credit", amount)
+	return nil
 }
 
-type TomatoTopping struct {
-	pizza IPizza
+func (w *WalletFacade) deductMoneyFromWallet(accountID string, securityCode int, amount int) error {
+	fmt.Println("Starting debit money from wallet")
+	err := w.account.checkAccount(accountID)
+	if err != nil {
+		return err
+	}
+
+	err = w.securityCode.checkCode(securityCode)
+	if err != nil {
+		return err
+	}
+	err = w.wallet.debitBalance(amount)
+	if err != nil {
+		return err
+	}
+	w.notification.sendWalletDebitNotification()
+	w.ledger.makeEntry(accountID, "debit", amount)
+	return nil
 }
 
-func (c *TomatoTopping) getPrice() int {
-	pizzaPrice := c.pizza.getPrice()
-	return pizzaPrice + 7
+// account
+
+type Account struct {
+	name string
 }
 
-type CheeseTopping struct {
-	pizza IPizza
+func newAccount(accountName string) *Account {
+	return &Account{
+		name: accountName,
+	}
 }
 
-func (c *CheeseTopping) getPrice() int {
-	pizzaPrice := c.pizza.getPrice()
-	return pizzaPrice + 10
+func (a *Account) checkAccount(accountName string) error {
+	if a.name != accountName {
+		return fmt.Errorf("Account Name is incorrect")
+	}
+	fmt.Println("Account Verified")
+	return nil
+}
+
+type SecurityCode struct {
+	code int
+}
+
+func newSecurityCode(code int) *SecurityCode {
+	return &SecurityCode{
+		code: code,
+	}
+}
+
+func (s *SecurityCode) checkCode(incomingCode int) error {
+	if s.code != incomingCode {
+		return fmt.Errorf("Security Code is incorrect")
+	}
+	fmt.Println("SecurityCode Verified")
+	return nil
+}
+
+type Wallet struct {
+	balance int
+}
+
+func newWallet() *Wallet {
+	return &Wallet{
+		balance: 0,
+	}
+}
+
+func (w *Wallet) creditBalance(amount int) {
+	w.balance += amount
+	fmt.Println("Wallet balance added successfully")
+	return
+}
+
+func (w *Wallet) debitBalance(amount int) error {
+	if w.balance < amount {
+		return fmt.Errorf("Balance is not sufficient")
+	}
+	fmt.Println("Wallet balance is Sufficient")
+	w.balance = w.balance - amount
+	return nil
+}
+
+type Ledger struct {
+}
+
+func (s *Ledger) makeEntry(accountID, txnType string, amount int) {
+	fmt.Printf("Make ledger entry for accountId %s with txnType %s for amount %d\n", accountID, txnType, amount)
+	return
+}
+
+type Notification struct {
+}
+
+func (n *Notification) sendWalletCreditNotification() {
+	fmt.Println("Sending wallet credit notification")
+}
+
+func (n *Notification) sendWalletDebitNotification() {
+	fmt.Println("Sending wallet debit notification")
 }
 
 func main() {
-	pizza := &VeggeMania{}
-	pizzawithChese := &CheeseTopping{
-		pizza: pizza,
-	}
-	pizzaWithCheseandTomato := &TomatoTopping{
-		pizza: pizzawithChese,
+	fmt.Println()
+	walletFacade := newWalletFacade("abc", 1234)
+	fmt.Println()
+
+	err := walletFacade.addMoneyToWallet("abc", 1234, 10)
+	if err != nil {
+		log.Fatalf("Error: %s\n", err.Error())
 	}
 
-	fmt.Println(pizzaWithCheseandTomato.getPrice())
+	fmt.Println()
+	err = walletFacade.deductMoneyFromWallet("abc", 1234, 5)
+	if err != nil {
+		log.Fatalf("Error: %s\n", err.Error())
+	}
 }
