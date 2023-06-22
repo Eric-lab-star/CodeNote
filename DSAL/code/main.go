54a7eda2c5f1118470fdd5a624ac562fc77cdc46
eric-lab-star <cyon256@icloud.com>
2023-06-22 20:22:57 +0900

    dsal

--- DSAL/code/main.go
+++ DSAL/code/main.go
@@ -2,38 +2,35 @@ package main
 
 import "fmt"
 
-type Computer interface {
-	Print()
-	SetPrinter(Printer)
-}
-
-type Mac struct {
-	printer Printer
-}
-
-func (m *Mac) Print() {
-	fmt.Println("Print request for mac")
-	m.printer.PrintFile()
-}
-
-func (m *Mac) SetPrinter(p Printer) {
-	m.printer = p
-}
-
-type Printer interface {
-	PrintFile()
-}
-
-type Epson struct {
-}
-
-func (p *Epson) PrintFile() {
-	fmt.Println("Printing by a Epson printer")
-}
-
 func main() {
-	mac := new(Mac)
-	epson := new(Epson)
-	mac.SetPrinter(epson)
-	mac.Print()
+	Start("abc")
+}
+
+type Combinations struct {
+	out []rune
+	in  []rune
+}
+
+func Start(input string) {
+	c := &Combinations{
+		in: []rune(input),
+	}
+
+	c.Combine(0)
+}
+
+// Combine ...
+func (c *Combinations) Combine(seed int) {
+
+	inLen := len(c.in)
+	for i := seed; i < inLen-1; i++ {
+		c.out = append(c.out, c.in[i])
+		fmt.Println(string(c.out))
+		c.Combine(i + 1)
+		c.out = c.out[:len(c.out)-1]
+	}
+
+	c.out = append(c.out, c.in[inLen-1])
+	fmt.Println(string(c.out))
+	c.out = c.out[:len(c.out)-1]
 }
