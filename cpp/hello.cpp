800b5e0421ef2bb8f7c8dfc22fc80e2b2a09c73d
kimkyungsub <cyon256@icloud.com>
2023-06-24 17:54:11 +0900

    codenote

---
+++ cpp/hello.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+#include <string>
+int main(){
+	string greeting = "hello\n" ; 
+	cout << greeting;
+	return 0;
+}
--- myJavaDoc/code/Hello.java
+++ myJavaDoc/code/Hello.java
@@ -1,7 +1,6 @@
 
 class Hello {
 	public static void main(String[] args) {
-
 		int bin = 0b1010101100;
 		System.out.printf("%d%n", bin);
 		System.out.printf("%x%n", bin);
