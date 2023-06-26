cdbd5803eb7b32490848adcc7e2c6297d8f92db8
kimkyungsub <cyon256@icloud.com>
2023-06-26 14:19:59 +0900

    tuple

--- DSAL/cmd/main.go
+++ DSAL/cmd/main.go
@@ -2,75 +2,19 @@ package main
 
 import "fmt"
 
-type Set struct{
-	integerMap map[int]bool
+/*
+Tuples are finit orderd sequences of objects. They can contain a mixture of other data types and are used to group related data into a data structure. In a relational database, a tuple is a row of a table. Tuples have a fixed size compared to lists, and are also faster.
+*/
+
+func h(x, y int) int{
+	return x*y
 }
 
-func (set *Set) New(){
-	set.integerMap = make(map[int]bool)
+func g(l,m int) (x,y int){
+	x= 2*l
+	y=4*m
+	return
 }
-
-func (set *Set) ContainElement(ele int) bool{
-	_, exists := set.integerMap[ele]
-	return exists
-}
-
-func (set *Set) AddElement(ele int){
-	if !set.ContainElement(ele){
-		set.integerMap[ele] = true
-	}
-}
-
-func (set *Set) deleteElement(ele int){
-	delete(set.integerMap, ele)
-
-}
-
-func (set *Set) InterSect(anotherSet *Set) *Set{
-	intersectSet := new(Set)
-	intersectSet.New()
-	for v := range set.integerMap{
-		if anotherSet.ContainElement(v) {
-			intersectSet.AddElement(v)
-		}	
-	}
-	return intersectSet
-
-}
-
-func (set *Set) Union (anotherSet *Set) *Set{
-	unionSet := &Set{}	
-	unionSet.New()
-	for v := range set.integerMap{
-		unionSet.AddElement(v)
-	}
-	for v := range anotherSet.integerMap{
-		unionSet.AddElement(v)
-	}
-	return unionSet
-
-}
-
-
 func main(){
-	set1:= new(Set)
-	set1.New()
-	set1.AddElement(2)
-	set1.AddElement(4)
-	set1.AddElement(6)
-	set1.AddElement(8)
-
-	set2 := new(Set)
-	set2.New()
-	set2.AddElement(3)
-	set2.AddElement(6)
-	set2.AddElement(9)
-	set2.AddElement(12)
-	union := set1.Union(set2)
-	fmt.Println(union)
-
-	intersect:=	set1.InterSect(set2)
-	fmt.Println(intersect)
-	
-	
+	fmt.Println(h(g(2,5)))
 }
