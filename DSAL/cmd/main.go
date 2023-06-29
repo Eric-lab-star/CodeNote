279325d419cd7f31696142cbc151221c9f99ad3a
kimkyungsub <cyon256@icloud.com>
2023-06-29 21:29:49 +0900

    binary search

--- DSAL/cmd/main.go
+++ DSAL/cmd/main.go
@@ -2,94 +2,121 @@ package main
 
 import "fmt"
 
+//binary search tree
+
 type Node struct {
-	key   int
-	value int
 	left  *Node
 	right *Node
+	key   int
+	count int
 }
 
 type Tree struct {
 	root *Node
 }
 
-func (t *Tree) Search(value int) bool {
-	return search(t.root, value)
+func (t *Tree) Search(key int) *Node {
+	return search(t.root, key)
+
 }
 
-func search(node *Node, value int) bool {
-	if node == nil {
-		return false
+func search(root *Node, key int) *Node {
+	if root == nil {
+		return nil
 	}
-
-	if node.value < value {
-		return search(node.right, value)
+	if root.key < key {
+		return search(root.right, key)
 	}
-
-	if node.value > value {
-		return search(node.left, value)
+	if root.key > key {
+		return search(root.left, key)
 	}
-
-	return true
+	return root
 }
 
-func (t *Tree) Insert(key, value int) {
-	node := &Node{key, value, nil, nil}
-	if t.root == nil {
-		t.root = node
-	}
-
-	if !t.Search(value) {
-		insert(t.root, node)
+func (t *Tree) Insert(key int) {
+	exist := t.Search(key)
+	if exist == nil {
+		node := &Node{nil, nil, key, 1}
+		if t.root == nil {
+			t.root = node
+		} else {
+			insert(t.root, node)
+		}
+	} else {
+		exist.count++
 	}
 }
 
-func insert(root, node *Node) {
-	if root.value < node.value {
+func insert(root, n *Node) {
+	if root.key < n.key {
 		if root.right == nil {
-			root.right = node
+			root.right = n
 		} else {
-			insert(root.right, node)
+			insert(root.right, n)
 		}
-	}
-	if root.value > node.value {
+	} else {
 		if root.left == nil {
-			root.left = node
+			root.left = n
 		} else {
-			insert(root.left, node)
+			insert(root.left, n)
 		}
 	}
+
 }
 
-func (t *Tree) PrintTree() {
-	print(t.root, 0)
+func (t *Tree) Delete(key int) {
+	delete(t.root, key)
 }
 
-func print(node *Node, level int) {
-	if node != nil {
-		format := ""
-		for i := 0; i < level; i++ {
-			format += "    "
-		}
-		level++
-		format += ">> "
-		print(node.right, level)
-		fmt.Printf(format+"%d\n", node.value)
-		print(node.left, level)
-
+func delete(node *Node, key int) *Node {
+	if node == nil {
+		return nil
 	}
+	if node.key < key {
+		node.right = delete(node.right, key)
+		return node
+	}
+	if node.key > key {
+		node.left = delete(node.left, key)
+		return node
+	}
+
+	if node.left == nil && node.right == nil {
+		node = nil
+		return nil
+	}
+
+	if node.left == nil {
+		node = node.right
+		return node
+	}
+
+	if node.right == nil {
+		node = node.left
+		return node
+	}
+
+	for leftmostRightNode := node.right; node != nil; leftmostRightNode = leftmostRightNode.left {
+		if leftmostRightNode.left == nil {
+			node.key = leftmostRightNode.key
+			node.right = delete(node.right, leftmostRightNode.key)
+			break
+		}
+	}
+
+	return node
+
 }
 
 func (t *Tree) Inorder() {
 	inorder(t.root)
 }
 
-func inorder(root *Node) {
-	if root != nil {
-		inorder(root.left)
-		fmt.Println(root.value)
-		inorder(root.right)
-
+func inorder(node *Node) {
+	if node != nil {
+		inorder(node.left)
+		fmt.Println(node.key)
+		inorder(node.right)
 	}
 }
 
@@ -99,7 +126,7 @@ func (t *Tree) Preorder() {
 
 func preorder(node *Node) {
 	if node != nil {
-		fmt.Println(node.value)
+		fmt.Println(node.key)
 		preorder(node.left)
 		preorder(node.right)
 	}
@@ -113,100 +140,65 @@ func postorder(node *Node) {
 	if node != nil {
 		postorder(node.left)
 		postorder(node.right)
-		fmt.Println(node.value)
+		fmt.Println(node.key)
+
 	}
 }
 
-func (t *Tree) MinNode() *int {
-	node := t.root
+func height(node *Node) int {
+
 	if node == nil {
-		return nil
+		return 0
 	}
-	for {
-		if node.left == nil {
-			return &node.value
-		}
-		node = node.left
+	return max(height(node.left), height(node.right)) + 1
+}
+func col(h int) int {
+	if h == 1 {
+		return 1
+	}
+	return col(h-1) + col(h-1) + 1
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	} else {
+		return b
 	}
 }
 
-func (t *Tree) MaxNode() *int {
-	treeNode := t.root
-	if treeNode == nil {
-		return nil
-	}
-	for {
-		if treeNode.right == nil {
-			return &treeNode.value
-		}
-		treeNode = treeNode.right
-	}
+func (t *Tree) Print() {
+	fmt.Println("---------------")
+	fmt.Println("---------------")
 }
-
-func (t *Tree) RemoveNode(value int) {
-	removeNode(t.root, value)
+func pow(a int, b int) int {
+	for i := 0; i < b; i++ {
+		a = a * a
+	}
+	return a
 }
-
-
-//removeNode!!!!
-func removeNode(node *Node, value int) *Node {
-	if node == nil {
-		return nil
+func print(m *[][]int, n *Node, col int, height int, row int) {
+	if n == nil {
+		return
 	}
-	if value < node.value {
-		node.left = removeNode(node.left, value)
-		return node
-	}
-
-	if value > node.value {
-		node.right = removeNode(node.right, value)
-		return node
-	}
-
-	//node without child
-
-	if node.left == nil && node.right == nil {
-		node = nil
-		return nil
-	}
-			
-
-	//node with 1 child
-	if node.left == nil {
-		node = node.right
-		return node
-	}
-	if node.right == nil {
-		node = node.left
-		return node
-	}
-	leftmostrightNode := node.right
-
-	//node with 2 children
-
-	for {
-		if leftmostrightNode != nil && leftmostrightNode.left != nil {
-			leftmostrightNode = leftmostrightNode.left
-		} else {
-			break
-		}
-	}
-
-	//copy to node
-	node.key, node.value = leftmostrightNode.key, leftmostrightNode.value
-	//remove leftmostrightNode
-	node.right = removeNode(node.right, node.key)
-	return node
+	print(m, n.left, col-pow(2, height-2), row+1, height-1)
+	print(m, n.left, col+pow(2, height-2), row+1, height-1)
 }
 
 func main() {
 	tree := &Tree{}
-	tree.Insert(4, 4)
-	tree.Insert(10, 10)
-	tree.Insert(10, 10)
-	tree.Insert(3, 3)
-	n := *tree.MaxNode()
-	fmt.Println(n)
-	tree.PrintTree()
-	tree.Inorder()
+	tree.Insert(5)
+	tree.Insert(2)
+	tree.Insert(8)
+	tree.Insert(10)
+	tree.Insert(1)
+	tree.Insert(3)
+	tree.Print()
+	h := height(tree.root)
+
+	if h != 0 {
+		for i := 0; i < h; i++ {
+
+		}
+	}
 }
